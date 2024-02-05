bd4397e993b1b0df0c9e7c8bd8b81ca5d11583b0
buermarc <buermarc2@gmail.com>
2024-02-06 00:05:57 +0100

    Allow to override env var optionally set default var

--- start_evaluate_grid.sh
+++ start_evaluate_grid.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-START=30
-STOP=60
-STEP=0.5
-EXPERIMENT_JSON="./experiments/s20003.json"
+START=${START:-0}
+STOP=${STOP:-0.6}
+STEP=${STEP:-0.01}
+EXPERIMENT_JSON=${EXPERIMENT_JSON:-"./experiments/s20003.json"}
 for i in $(python -c "import numpy as np; [print(i) for i in np.arange(${START}, ${STOP}, ${STEP})]"); do
     cd /home/orb/repos/kinect_mocap_studio/
     echo ./build/evaluate -e ${EXPERIMENT_JSON} -r 0 -p 0 -f 1 -m $i -x 1
