6f6a89e67acc7c8a577c60a736e39946d67449d4
buermarc <buermarc2@gmail.com>
2024-02-20 22:13:54 +0100

    Update of parallel script

--- new_parallel.sh
+++ new_parallel.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-find ./experiments -name "*.json" | parallel ./build/evaluate  -e {} -r 0 -p 0 -f  1 -m 100 -n 105 -s 5 --early_exit 1
+find ./experiments -name "*.json" | parallel ./build/evaluate  -e {} -r 0 -p 0 -f  1 -m 0 -n 100 -s 5 --early_exit 1 -t 1 &
+find ./experiments -name "*.json" | parallel ./build/evaluate  -e {} -r 0 -p 0 -f  1 -m 0 -n 100 -s 5 --early_exit 1 -t 2 &
+find ./experiments -name "*.json" | parallel ./build/evaluate  -e {} -r 0 -p 0 -f  1 -m 0 -n 100 -s 5 --early_exit 1 -t 3 &
+find ./experiments -name "*.json" | parallel ./build/evaluate  -e {} -r 0 -p 0 -f  1 -m 0 -n 100 -s 5 --early_exit 1 -t 4 &
