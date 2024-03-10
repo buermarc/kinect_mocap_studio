3da8775c272bb77960ccd2e3f1d39677d1ae369c
buermarc <buermarc2@gmail.com>
2024-03-11 00:25:32 +0100

    Update new_parallel.sh

--- new_parallel.sh
+++ new_parallel.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-find ./experiments -name "*.json" | parallel ./build/evaluate  -e {} -r 0 -p 0 -f  1 -m 0 -n 4.8 -s 0.2 --early_exit 1 -t 1 &
-find ./experiments -name "*.json" | parallel ./build/evaluate  -e {} -r 0 -p 0 -f  1 -m 0 -n 4.8 -s 0.2 --early_exit 1 -t 2 &
-find ./experiments -name "*.json" | parallel ./build/evaluate  -e {} -r 0 -p 0 -f  1 -m 0 -n 4.8 -s 0.2 --early_exit 1 -t 3 &
-find ./experiments -name "*.json" | parallel ./build/evaluate  -e {} -r 0 -p 0 -f  1 -m 0 -n 4.8 -s 0.2 --early_exit 1 -t 4 &
+find ./experiments -name "*.json" | parallel ./build/evaluate  -e {} -r 0 -p 0 -f  1 -m 0 -n 9.8 -s 0.2 --early_exit 1 -t 1 &
+find ./experiments -name "*.json" | parallel ./build/evaluate  -e {} -r 0 -p 0 -f  1 -m 0 -n 9.8 -s 0.2 --early_exit 1 -t 2 &
+find ./experiments -name "*.json" | parallel ./build/evaluate  -e {} -r 0 -p 0 -f  1 -m 0 -n 9.8 -s 0.2 --early_exit 1 -t 3 &
+find ./experiments -name "*.json" | parallel ./build/evaluate  -e {} -r 0 -p 0 -f  1 -m 0 -n 9.8 -s 0.2 --early_exit 1 -t 4 &
 
-find ./experiments -name "*.json" | parallel ./build/evaluate  -e {} -r 0 -p 0 -f  1 -m 5.0 -n 100.0 -s 5 --early_exit 1 -t 1 &
-find ./experiments -name "*.json" | parallel ./build/evaluate  -e {} -r 0 -p 0 -f  1 -m 5.0 -n 100.0 -s 5 --early_exit 1 -t 2 &
-find ./experiments -name "*.json" | parallel ./build/evaluate  -e {} -r 0 -p 0 -f  1 -m 5.0 -n 100.0 -s 5 --early_exit 1 -t 3 &
-find ./experiments -name "*.json" | parallel ./build/evaluate  -e {} -r 0 -p 0 -f  1 -m 5.0 -n 100.0 -s 5 --early_exit 1 -t 4 &
+find ./experiments -name "*.json" | parallel ./build/evaluate  -e {} -r 0 -p 0 -f  1 -m 10.0 -n 150.0 -s 5 --early_exit 1 -t 1 &
+find ./experiments -name "*.json" | parallel ./build/evaluate  -e {} -r 0 -p 0 -f  1 -m 10.0 -n 150.0 -s 5 --early_exit 1 -t 2 &
+find ./experiments -name "*.json" | parallel ./build/evaluate  -e {} -r 0 -p 0 -f  1 -m 10.0 -n 150.0 -s 5 --early_exit 1 -t 3 &
+find ./experiments -name "*.json" | parallel ./build/evaluate  -e {} -r 0 -p 0 -f  1 -m 10.0 -n 150.0 -s 5 --early_exit 1 -t 4 &
